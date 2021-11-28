74933663472191873a017f006bd659573190dcf4
Guilherme fernandes gomes <guilhermefgs@gmail.com>
2021-11-28 17:34:47 -0300

    #nome e idade

--- exercicios/ex002.py
+++ exercicios/ex002.py
@@ -1,11 +1,12 @@
+#nome e idade
 nome = input('digite seu nome:')
 print('É um prazer te conhecer, {}!'.format(nome))
 ano = int(input('em que ano vc nasceu ?'))
 idadevirtual = (2021 - ano)
-simounao = input('Sua idade é  {}?'.format(idadevirtual))
+idade = input('Sua idade é  {}'.format(idadevirtual))
 
-if simounao == 'sim':
-    print('parabens')
-else:
-   mes = input('qual mes vc nasceu?')
-   dia = input('qual dia vc nasceu?')
+
+
+
+
+
